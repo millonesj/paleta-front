1b14b25c85b1dae2eb2345343efee358de6b2464
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-11-26 22:27:27 -0500

    refactor(router): Add Reach Router and configure routes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,11 @@
 import React from "react";
-import AppToolbar from "./components/appToolbar";
-import ColorSetter from "./components/colorSetter";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import PaletteList from "./components/paletas";
-import ColorProvider from "./colorContext/colorProvider";
+import Router  from "./Router"
 
-import Chat from "./components/Chat";
-import Store from "./contexts/Store";
-import Register from "./components/Register"
 export default function App() {
+
   return (
     <div>
-      <ColorProvider>
-        <Grid container>
-          <Grid item xs={12} style={{ height: "50px" }}>
-            <AppToolbar />
-          </Grid>
-          <Grid item xs={3}>
-            <Paper
-              style={{
-                display: "flex",
-                marginInline: "4px",
-                height: "87vh",
-                padding: "8px",
-                backgroundColor: "#e0e0e0"
-              }}
-            >
-              <Store>
-                <Chat></Chat>
-              </Store>
-            </Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper
-              style={{
-                marginInline: "4px",
-                height: "87vh",
-                padding: "8px",
-                backgroundColor: "#e0e0e0"
-              }}
-            >
-              <ColorSetter />
-            </Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper
-              style={{
-                marginInline: "4px",
-                height: "87vh",
-                padding: "8px",
-                backgroundColor: "#e0e0e0"
-              }}
-            >
-              <Register/>
-              <PaletteList />
-            </Paper>
-          </Grid>
-        </Grid>
-      </ColorProvider>
+      <Router></Router>
     </div>
   );
 }
