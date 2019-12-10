936ae05e80aa37cba2f43931814bcbb132c32c89
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-12-09 22:56:36 -0500

    refactor(.env): Add custom environment variables

--- src/hooks/ChatContext.js
+++ src/hooks/ChatContext.js
@@ -38,10 +38,10 @@ const sendChatAction = value => {
 const ChatContextProvider = props => {
   const [proyectChat, setProyectChat] = useState({});
   const [allChats, dispatch] = useReducer(reducer, initState);
-  const user = 'Jhonny' + Math.random(100).toFixed(2);
+  const user = '';
 
   if (!socket) {
-    socket = io(':3001', {
+    socket = io(process.env.REACT_APP_URL_CHAT, {
       query: {
         token: getToken()
       }
